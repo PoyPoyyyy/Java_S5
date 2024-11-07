9d9a1366edf6596d742243fe441bce6e7b484249
Paco Poyo-Dufour <96145167+PoyPoyyyy@users.noreply.github.com>
2024-11-07 17:27:09 +0100

    Save

--- TP_Samourai/src/personnages/principaux/Ronins.java
+++ TP_Samourai/src/personnages/principaux/Ronins.java
@@ -1,5 +1,13 @@
 package personnages.principaux;
+import personnages.*;
 
-public class Ronins {
-
-}
+public class Ronins extends Humain {
+	private int honneur;
+	
+	public Ronins() {
+		public Yakuza(String nom, int argent, String boisson, String clan, int reputation) {
+			super(nom, argent, boisson);
+			this.clan = clan;
+			this.reputation = reputation;
+		}
+		
--- TP_Samourai/src/personnages/principaux/Yakuza.java
+++ TP_Samourai/src/personnages/principaux/Yakuza.java
@@ -34,4 +34,19 @@ public class Yakuza extends Humain {
 		message += "J'ai gagné un duel";
 		this.parler(message);
 	}
+	
+	public int perdreDuel() {
+		int argent = this.getArgent();
+		this.perdreArgent(argent);
+		this.reputation -= 1;
+		String message = new String();
+		message += "J'ai perdu un duel";
+		this.parler(message);
+		return argent;
+	}
+	
+	@Override
+	public void direBonjour() {
+		
+	}
 }
