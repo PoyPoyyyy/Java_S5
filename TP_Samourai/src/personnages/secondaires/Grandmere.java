ae8705f46c3c3f64cc97cc87c76fd9a2b3dda978
Paco Poyo-Dufour <96145167+PoyPoyyyy@users.noreply.github.com>
2024-11-08 11:06:03 +0100

    Save

--- TP_Samourai/src/personnages/secondaires/Grandmere.java
+++ TP_Samourai/src/personnages/secondaires/Grandmere.java
@@ -1,5 +1,55 @@
 package personnages.secondaires;
+import personnages.*;
+import java.util.Random ;
 
-public class Grandmere {
-
+public class Grandmere extends Humain{
+	private Humain[] memoire = new Humain[30];
+	
+	public Grandmere(String nom, int argent, String boisson) {
+		super(nom, argent, boisson);
+		this.memoire = new Humain[30];
+	}
+	
+	private String HumainHasard() {
+		Random random = new Random();
+		String message = new String();
+		switch (random.nextInt(1, 9)) {
+			case 1:
+				message += "Commercant";
+				break;
+			case 2:
+				message += "Ronins";
+				break;
+			case 3:
+				message += "Samourai";
+				break;
+			case 4:
+				message += "Yakuza";
+				break;
+			case 5:
+				message += "Colporteur";
+				break;
+			case 6:
+				message += "Ninja";
+				break;
+			case 7:
+				message += "Traitre";
+				break;
+			case 8:
+				message += "Humain";
+				break;
+			default:
+				message += " ";
+				break;
+		}
+		return message;
+		
+		public void faireConnaissanceAvec (Humain humain) {
+			
+		}
+		
+		public void ragoter() {
+			
+		}
+	}
 }
--- TP_Samourai/src/personnages/secondaires/Traitre.java
+++ TP_Samourai/src/personnages/secondaires/Traitre.java
@@ -1,5 +1,47 @@
 package personnages.secondaires;
+import personnages.principaux.*;
+import personnages.*;
 
-public class Traitre {
+public class Traitre extends Samourai {
+	private int niveauTraitrise;
+	
+	public Traitre(String nom, String boisson, int argent, String seigneur) {
+		super(nom, boisson, argent, seigneur);
+		this.niveauTraitrise = 0;
+	}
+	
+	public void extorquer(Commercant commercant) {
+		if (this.niveauTraitrise < 3) {
+			int argent = commercant.seFaireExtorquer();
+			this.gagnerArgent(argent);
+			this.niveauTraitrise ++;
+			String message = new String();
+			message += "Je viens d'extorquer un commercant";
+			this.parler(message);
+		} 
 
+	}
+	
+	@Override
+	public void direBonjour() {
+        super.direBonjour();
+        String message = new String();
+        message += "Mon niveau de maitrise " + this.niveauTraitrise;
+        this.parler(message);
+	}
+	
+	public void faireLeGentil(Humain humain, int argent) {;
+		this.perdreArgent(argent);
+		humain.gagnerArgent(argent);
+		String message = new String();
+		message += "Je fais ami ami";
+		this.parler(message);
+		float traitrise = argent / 10;
+		if (this.niveauTraitrise < traitrise) {
+			this.niveauTraitrise = 0;
+		}
+		else {
+			this.niveauTraitrise -= traitrise;
+		}
+	}
 }
